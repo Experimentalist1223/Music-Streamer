5bdd11625f48775279e606e8b72cfe54f5fef0dd
TheStrayCoder <104091630+TheStrayCoder@users.noreply.github.com>
2022-04-21 14:43:37 +0530

    Update extra.py

--- handlers/extra.py
+++ handlers/extra.py
@@ -1,3 +1,15 @@
+import asyncio
+from time import time
+from datetime import datetime
+from helpers.filters import command
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from config import (
+    BOT_USERNAME,
+    OWNER_USERNAME,   
+)
+
+
 @Client.on_message(command(["start"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_text("**Thanks for adding me im your Group ❤️ Now promote me as a admin with needed powers otherwise I am not able to work properly !!**",
