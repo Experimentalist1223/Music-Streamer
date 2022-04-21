44e8f06c7680e0c00b89a787355ca8bd8659e95c
TheStrayCoder <104091630+TheStrayCoder@users.noreply.github.com>
2022-04-21 10:43:31 +0530

    Update callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -10,12 +10,7 @@ from config import (
 async def start_op(_, query: CallbackQuery):
     await query.answer("Bot Started")
     await query.edit_message_text(
-              f"""Hello, Welcome {message.from_user.mention()}
-
-I am powerful easy to use TeleGram Super Bot. I can play high quality and unbreakable Songs in your group voice chat. Just add me and promote with needed powers.
-
-Use Inline buttons for more !!
-For Help : @StrayCoderSupport
+              f"""Hello, Welcome {message.from_user.mention()}\n\nI am powerful easy to use TeleGram Super Bot. I can play high quality and unbreakable Songs in your group voice chat. Just add me and promote with needed powers.\n\nUse Inline buttons for more !!\nFor Help : @StrayCoderSupport
 """,
         reply_markup=InlineKeyboardMarkup(
             [
