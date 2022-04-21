35518532991f69b8cb699c936beeeaaae505f1bb
TheStrayCoder <104091630+TheStrayCoder@users.noreply.github.com>
2022-04-21 14:28:15 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -43,22 +43,6 @@ async def ping_pong(c: Client, message: Message):
     await m_reply.edit_text("**× I am Alive ×**\n\n@StrayCoder 📡")
 
 
-@Client.on_message(command(["start"]) & filters.group & ~filters.edited)
-async def help(client: Client, message: Message):
-    await message.reply_text("**Thanks for adding me im your Group ❤️ Now promote me as a admin with needed powers otherwise I am not able to work properly !!**",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "⚙️ Source Code", url=f"https://github.com/TheStrayCoder/Music-Streamer")
-                ],[
-                    InlineKeyboardButton("📨 Support", url=f"https://t.me/StrayCoderSupport"),
-                    InlineKeyboardButton("📨 Updates", url=f"https://t.me/StrayCoder")
-                  ],
-            ]
-        ),
-    )
-
 
 @Client.on_message(command(["repo"]) & ~filters.edited)
 async def help(client: Client, message: Message):
