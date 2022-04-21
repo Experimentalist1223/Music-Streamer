23f88517d480129a85f875f59e62fecbe7358db6
TheStrayCoder <104091630+TheStrayCoder@users.noreply.github.com>
2022-04-21 14:54:37 +0530

    Update extra.py

--- handlers/extra.py
+++ handlers/extra.py
@@ -10,7 +10,7 @@ from config import (
 )
 
 
-@Client.on_message(command(["start"]) & filters.group & ~filters.edited)
+@Client.on_message(command(["start", f"start@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_text("**Thanks for adding me im your Group ❤️ Now promote me as a admin with needed powers otherwise I am not able to work properly !!**",
         reply_markup=InlineKeyboardMarkup(
