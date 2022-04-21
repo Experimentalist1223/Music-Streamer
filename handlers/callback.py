259a273c467e6f30a3043cc6253ce3bdd2c5d3df
TheStrayCoder <104091630+TheStrayCoder@users.noreply.github.com>
2022-04-21 14:40:49 +0530

    Update callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -61,18 +61,3 @@ async def cbcmd(_, query: CallbackQuery):
     )
 
 
-@Client.on_message(command(["start"]) & filters.group & ~filters.edited)
-async def help(client: Client, message: Message):
-    await message.reply_text("**Thanks for adding me im your Group ❤️ Now promote me as a admin with needed powers otherwise I am not able to work properly !!**",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "⚙️ Source Code", url=f"https://github.com/TheStrayCoder/Music-Streamer")
-                ],[
-                    InlineKeyboardButton("📨 Support", url=f"https://t.me/StrayCoderSupport"),
-                    InlineKeyboardButton("📨 Updates", url=f"https://t.me/StrayCoder")
-                  ],
-            ]
-        ),
-    )
